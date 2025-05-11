9b2be6c5975cb5820b32de2b53a16805cd76a9ed
MaximumCell <alihassanwinner@gmail.com>
2025-05-11 21:15:23 +0500

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-};
 
-app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
